a59b4a373c9eafeeed1d690d1e724b7d7dcaeac9
nienkek2101 <nienke.kapers@gmail.com>
2021-12-21 13:51:16 +0100

    svg en back-button doen het

--- src/pages/subredditSpecification/SubredditSpecification.js
+++ src/pages/subredditSpecification/SubredditSpecification.js
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from "react-router-dom";
+import { Link, useHistory, useParams } from "react-router-dom";
+import backButton from '../../assets/back.svg';
+import './SubredditSpecification.css';
 
 function SubredditSpecification() {
     const [ dataSubreddit, setDataSubreddit ] = useState([]);
     const { subredditName } = useParams();
+    const history = useHistory();
+
+    function backToHome() {
+        history.push('/');
+    }
 
     useEffect(() => {
 
         async function fetchData() {
+
             try {
                 // const result = await axios.get('https://www.reddit.com/r/tumblr/about.json');
                 const result = await axios.get(`https://www.reddit.com/r/${subredditName}/about.json`);
@@ -28,7 +36,10 @@ function SubredditSpecification() {
             {/*<img src={dataSubreddit.mobile_banner_image} />*/}
             <p>Title: {dataSubreddit.title}</p>
             <p>Description: {dataSubreddit.public_description}</p>
-            <p>Number of subscribers: {dataSubreddit.subscribers}</p>
+            <p>Number of subscribers: {Object.keys(dataSubreddit).length > 0 && dataSubreddit.subscribers.toLocaleString()}</p>
+            {/*<Link to={backToHome}>Terug naar home</Link>*/}
+            <button type="button" onClick={backToHome}><img src={backButton} alt="back-button" /> Ga terug</button>
+
         </>
     );
 
